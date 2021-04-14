c04142f72783403188cfb01d5bb796010203fcd9
loganb87 <54338709+loganb87@users.noreply.github.com>
2021-04-13 20:00:23 -0400

    2.5 Code Quality
    Implement linters to clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log ("Hi! Welcome to My Portfolio")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Hi! Welcome to My Portfolio')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
